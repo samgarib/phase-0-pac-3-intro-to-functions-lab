bcd6874bdac1fda6021d434a8604fe916a1f8d47
samantha debenedictis <samdebenedictis@aol.com>
2022-05-31 17:33:09 -0400

    commit tree 034f02d44b9c273fa35585614399809bcd200901

---
+++ index.js
@@ -0,0 +1,21 @@
+function shout(string) {
+    // todo
+  }
+
+  function shout(string) {
+    return string;
+  }
+
+  "Hello!".toUpperCase(); // 'HELLO!'
+
+  function shout(string) {
+    return string.toUpperCase();
+  }
+
+  function whisper(string){
+     return string.toLowerCase() 
+  }
+
+  function logShout(string){
+      console.log(string.toLowerCase)
+  }
